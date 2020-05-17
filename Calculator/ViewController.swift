c08c540eb08be14a41714e74ddfac022acf6101f
Ville-Matti Hakanpää <ville-matti.hakanpaa@futuriot.com>
2020-05-17 17:38:27 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Ville-Matti Hakanpää on 17.5.2020.
+//  Copyright © 2020 Ville-Matti Hakanpää. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
